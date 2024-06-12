48266a91f8ad33e8a4baa5398e016cb9544cafa6
axlj45 <11035621+axlj45@users.noreply.github.com>
2024-06-12 17:08:21 -0400

    Feature/trade summary (#1)
    * refactor: migrate db models to dataclasses
    
    * feat(trade-summary): working trade summary poc
    
    * feat(complete-the-trade): added cli for closing and summarizing trades

--- pytrader/model/signal.py
+++ pytrader/model/signal.py
@@ -1,27 +1,19 @@
+from dataclasses import dataclass
 from datetime import datetime
 from pytrader.utils import localtime
 
 
+@dataclass
 class SignalModel:
-    def __init__(
-        self,
-        id: str,
-        symbol: str,
-        action: str,
-        date: datetime,
-        strategy: str,
-        metadata: dict | None = None,
-        execute_on: datetime | None = None,
-        order_id: str | None = None,
-    ):
-        self.id = id
-        self.symbol = symbol
-        self.action = action
-        self.date = date
-        self.strategy = strategy
-        self.metadata = metadata
-        self.execute_on = execute_on
-        self.orderId = order_id
+    id: str
+    symbol: str
+    action: str
+    date: datetime
+    strategy: str
+    metadata: dict | None = None
+    executeOn: datetime | None = None
+    orderId: str | None = None
+    resolvedOrder: dict | None = None
 
     def to_dict(self):
         result = {
@@ -29,25 +21,12 @@ class SignalModel:
             "action": self.action,
             "date": self.date,
             "strategy": self.strategy,
-            "executeOn": self.execute_on,
+            "executeOn": self.executeOn,
             "orderId": self.orderId,
             "metadata": self.metadata or {},
+            "resolvedOrder": self.resolvedOrder or {},
         }
-
         return result
-
-    @staticmethod
-    def from_dict(id, data):
-        return SignalModel(
-            id,
-            data["symbol"],
-            data["action"],
-            data["date"],
-            data["strategy"],
-            data["metadata"],
-            data["executeOn"],
-            data["orderId"],
-        )
 
     @staticmethod
     def create_signal(
@@ -59,6 +38,4 @@ class SignalModel:
         execute_on: datetime,
     ):
         date = localtime.today()
-        return SignalModel(
-            id, symbol, action, date, strategy, metadata, execute_on=execute_on
-        )
+        return SignalModel(id, symbol, action, date, strategy, metadata, executeOn=execute_on)
